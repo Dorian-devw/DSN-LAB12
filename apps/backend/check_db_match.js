d5f88b3d070c42f180b31c621e3ee48af30270e6
Dorian-devw <frank.sinca@tecsup.edu.pe>
2026-06-12 23:09:03 -0500

    Remove unnecessary check scripts

--- apps/backend/check_db_match.js
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-prisma.match.findFirst().then(m => {
-  console.log(m);
-  prisma.$disconnect();
-});
--- apps/backend/check_football_data.js
+++
@@ -1,6 +0,0 @@
-fetch('https://api.football-data.org/v4/competitions/WC/matches', {
-  headers: { 'X-Auth-Token': '0b299191a3b845efa8dab306b25d058e' }
-})
-.then(res => res.json())
-.then(data => console.log(JSON.stringify(data, null, 2)))
-.catch(console.error);
